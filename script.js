737cc8e6bfb221dfd5dd6dd1f153d4b0b191a191
Nathaniel Campbell <nathanielcampbell2002@gmail.com>
2023-01-08 12:44:50 -0500

    Hotfix

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const menu = document.getElementById('mobile-menu');
 const btnOpen = document.getElementById('btn-menu-open');
 const btnClose = document.getElementById('btn-menu-close');
 
-function toggleMenu() {
-	menu.ariaHidden = !JSON.parse(menu.ariaHidden);
+function toggleMenu(hide) {
+	menu.ariaHidden = hide !== undefined ? hide : !JSON.parse(menu.ariaHidden);
 }
 
-btnOpen.addEventListener('click', toggleMenu);
-btnClose.addEventListener('click', toggleMenu);
+btnOpen.addEventListener('click', toggleMenu.bind(this, false));
+btnClose.addEventListener('click', toggleMenu.bind(this, true));
 menu.addEventListener('click', function (ev) {
-	if (!ev.target.closest('nav') || ev.target.tagName === 'A') toggleMenu();
+	if (!ev.target.closest('nav') || ev.target.tagName === 'A') toggleMenu(true);
 });
